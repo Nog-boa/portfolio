be6ca11f5a9feaaf5f497f0aec69458fa3180e07
nog <rladpshr95@gmail.com>
2024-09-28 16:20:19 +0900

    first

---
+++ app/chat/page.jsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [question, setQuestion] = useState("");
+
+  const SendMessage = async () => {
+    setMessages([...messages, { text: question, sender: "user" }]);
+
+    try {
+      const response = await axios.post("http://localhost:5050/chat", {
+        question,
+      });
+
+      setMessages([
+        ...messages,
+        { text: question, sender: "user" },
+        { text: response.data.response, sender: "server" },
+      ]);
+    } catch (error) {
+      console.error("Error sending message:", error);
+      setMessages([
+        ...messages,
+        { text: question, sender: "user" },
+        { text: "에러 발생", sender: "server" },
+      ]);
+    }
+
+    setQuestion("");
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-start bg-dark text-white pt-0 mt-0">
+      <h3 className="text-4xl font-bold mt-0 pt-0 mb-4">
+        이력서를 기반으로 한 AI-Chatbot 입니다.
+      </h3>
+      <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 mb-8">
+        궁금한 것을 채팅으로 물어보세요.
+        <br />
+        이력서를 기준으로 AI-Chatbot이 알려줄 것입니다.
+      </p>
+
+      <div className="w-full max-w border-2 border-accent rounded-lg p-4 mt-8">
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold text-center">Chat</h1>
+        </div>
+
+        <div className="flex flex-col space-y-4 h-64 overflow-y-auto mb-4">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`p-2 rounded-md max-w-xs ${
+                message.sender === "user"
+                  ? "ml-auto bg-accent text-black"
+                  : "mr-auto bg-gray-700"
+              }`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex">
+          <input
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            className="flex-grow p-2 rounded-l-lg bg-gray-800 border-none focus:outline-none text-white"
+            placeholder="메세지를 입력해주세요"
+          />
+          <button
+            onClick={SendMessage}
+            className="bg-gray text-gray px-4 rounded-r-lg"
+          >
+            Send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ app/resume/page.jsx
@@ -0,0 +1,204 @@
+"use client";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+//about data
+const about = {
+  title: "About me",
+  description:
+    "I value the availability of the service most and I am interested in security and network, constantly studying.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Enog Kim",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "010 1234 5678",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "1 year",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Korean",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "rlashrdlsrk@naver.com",
+    },
+  ],
+};
+
+// experience data
+const experience = {
+  // icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description:
+    "I value the availability of the service most and I am interested in security and network, constantly studying.",
+  items: [
+    {
+      company: "Shieldus",
+      position: "Security opearation",
+      duration: "09.2023 ~",
+    },
+  ],
+};
+
+// skil data
+const skills = {
+  title: "My skils",
+  description:
+    "저는 이러한 스킬들을 가지고 있습니다. 추후에 설명을 붙이겠습니다.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "htmll 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <FaReact />,
+      name: "react.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwind.css",
+    },
+  ],
+};
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
+const Reusme = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          {/* content */}
+          <div className="min-h-[70vh] w-full">
+            {/* experience */}
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul cassName="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="fex items-center gap-3">
+                            {/* dot 여기서부터 하면 된다.*/}
+                            <span></span>
+                            <p>{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            {/* education */}
+            <TabsContent value="education" className="w-full h-full">
+              education
+            </TabsContent>
+            {/* skills */}
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+            {/* about */}
+            <TabsContent value="about" className="w-full">
+              about
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Reusme;
